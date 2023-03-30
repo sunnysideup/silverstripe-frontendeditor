ced09798e8922f710b06f3ac374e5f3518f7abad
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2023-03-30 22:45:45 +1300

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/model/FrontEndEditorExplanationsBaseClass.php
+++ code/model/FrontEndEditorExplanationsBaseClass.php
@@ -2,6 +2,20 @@
 
 class FrontEndEditorExplanationsBaseClass extends DataObject
 {
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'FrontEndEditorExplanationsBaseClass';
+
     private static $db = array(
         "ObjectClassName" => "Varchar(100)",
         "ObjectFieldName" => "Varchar(100)",
